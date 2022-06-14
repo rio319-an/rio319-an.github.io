9b20aee07c231fe033cd96b8b9e6da935e5d906d
An Seung Woo <78468001+rio319-an@users.noreply.github.com>
2022-06-14 17:57:48 +0900

    Delete mblock directory

--- mblock/qnaScript.js
+++
@@ -1,6 +0,0 @@
-const list = document.querySelector("section .board-main .right ul li"),
-    title = list.querySelector(".list-title"),
-    content = list.querySelector(".list-content"),
-    icon = title.querySelector("i");
-
-title.addEventListener("click", ()=> title.classList.toggle("active"));
