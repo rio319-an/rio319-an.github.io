7b20b16f643040edfe5ed484cdb526957aa510da
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-07-05 15:09:43 +0900

    member-detail

--- mblock/edit_image.js
+++ mblock/edit_image.js
@@ -1,3 +1,4 @@
+//이미지 변경
 const defaultBtn = document.querySelector("#selectImg");
 const customBtn = document.querySelector("main .right .profile .img button");
 const img = document.querySelector("main .right .profile .img img");
@@ -20,3 +21,20 @@ defaultBtn.addEventListener("change",function(){
     img.classList.remove("active");
 
 });
+
+//스피너
+const optionMenu = document.querySelector("main .right .select-menu"),
+        selectBtn = optionMenu.querySelector(".select-btn"),
+        options = optionMenu.querySelectorAll(".option"),
+        sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+    })
+})
